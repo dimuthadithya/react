9316f1a11f8545511df1a550448bfc97b2d39529
Dimuth Adithya <dimuthx353@gmail.com>
2024-08-22 21:56:38 +0530

    9. Local Storage Store All Tasks

--- 8. Project TaskMate/taskmate/src/App.js
+++ 8. Project TaskMate/taskmate/src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import { Header } from './components/Header';
 import { AddTask } from './components/AddTask';
 import { ShowTask } from './components/ShowTask';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState(
+    JSON.parse(localStorage.getItem('tasklist')) || []
+  );
   const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem('tasklist', JSON.stringify(taskList));
+  }, [taskList]);
 
   return (
     <div className="App">
