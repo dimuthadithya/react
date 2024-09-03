d32b71a849a240febe763d06aa8a5016e6c4e79f
Dimuth Adithya <dimuthx353@gmail.com>
2024-09-03 15:44:38 +0530

    3. Project Setup

---
+++ 11. Movie API Project Cinemate/cinemate/src/components/index.js
@@ -0,0 +1,3 @@
+export { Header } from './Header.js';
+export { Footer } from './Footer.js';
+export { Card } from './Card.js';
---
+++ 11. Movie API Project Cinemate/cinemate/src/pages/MovieDetail.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const MovieDetail = () => {
+  return <main>Movie List</main>;
+};
