687489571b62f9f6e7501f1bd81772abb5cb07f2
Dimuth Adithya <dimuthx353@gmail.com>
2024-09-07 18:48:38 +0530

    10. API Fetch Movie List

--- 11. Movie API Project Cinemate/cinemate/src/pages/MovieList.js
+++ 11. Movie API Project Cinemate/cinemate/src/pages/MovieList.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card } from '../components';
 
 export const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  const url =
+    'https://api.themoviedb.org/3/movie/now_playing?api_key=25939239a6cdd327d9c31aef2af60d5c&language=en-US';
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=25939239a6cdd327d9c31aef2af60d5c&language=en-US'
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    }
+
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <section className="mx-auto max-w-7xl py-7">
         <div className="flex flex-wrap justify-start ">
-          <Card />
+          {movies.map((movie) => {
+            return <Card key={movie.id} movie={movie} />;
+          })}
         </div>
       </section>
     </main>
